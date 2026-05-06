667051ba39158394190565fefc7d65f8ad07fd4e
HarryOreLara <harryorelara@gmail.com>
2026-05-06 18:53:29 -0500

    feature/header/ Se complementa los headers de productos

--- src/app/pages/products/list-products/commons/product-header-info/product-header-info.component.ts
+++ src/app/pages/products/list-products/commons/product-header-info/product-header-info.component.ts
@@ -14,6 +14,33 @@ export class ProductHeaderInfoComponent {
     return this.products.filter((product) => product.stock < 10).length;
   }
 
+  public get lowStockPercentage(): number {
+    if (!this.products.length) return 0;
+
+    const lowStockCount = this.products.filter(
+      (product) => product.stock < 10,
+    ).length;
+
+    return Number(((lowStockCount / this.products.length) * 100).toFixed(2));
+  }
+
+  get productsLastMonth(): number {
+    if (!this.products?.length) return 0;
+
+    const now = new Date();
+
+    const lastMonth = new Date();
+    lastMonth.setMonth(now.getMonth() - 1);
+
+    return this.products.filter((product) => {
+      if (!product.createdAt) return false;
+
+      const createdAt = new Date(product.createdAt);
+
+      return createdAt >= lastMonth && createdAt <= now;
+    }).length;
+  }
+
   public get totalInventoryValue(): number {
     return (
       this.products?.reduce((total, product) => {
